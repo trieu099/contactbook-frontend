c5ac64a82557b976aac4c45ac1d537eec381129f
trieu099 <trieu099@gmail.com>
2022-10-28 21:39:29 +0700

    Create an add page

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
         component: () => import('@/views/ContactEdit.vue'),
         props: (route) => ({ contactId: parseInt(route.params.id) })
     },
+    {
+        path: "/contacts",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+      },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
